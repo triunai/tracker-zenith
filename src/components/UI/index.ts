0c85bf7fada00007b5453e775272fd4107d38866
Khumeren <khucode@gmail.com>
2025-04-03 12:45:50 +0800

    fix deployment plsss

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,4 +1,4 @@
 // Re-export UI components to ensure consistent imports regardless of case
-export { Toaster } from './toaster';
+export { Toaster } from './toaster.tsx';
 export { ToastProvider, useToast } from './use-toast';
 export { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from './tooltip'; 
