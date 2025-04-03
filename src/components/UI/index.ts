e1719691e2735f7f9f917b37d6e1dbfe9542da6e
Khumeren <khucode@gmail.com>
2025-04-03 13:01:12 +0800

    fix

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,4 +1,4 @@
 // Re-export UI components to ensure consistent imports
 export { ToastProvider, useToast, toast } from '@/components/UI/use-toast.ts';
-export { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from './tooltip';
-export { Toaster } from './toaster';
+export { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/UI/tooltip';
+export { Toaster } from '@/components/UI/toaster';
